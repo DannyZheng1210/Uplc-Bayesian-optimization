0360c5c5b68611d9a5cde9cd029da531e7bec65d
Dongyang <zhengdongyang1996@hotmail.com>
2026-05-28 12:52:09 +0100

    Delete read_data.py

--- read_data.py
+++
@@ -1,7 +0,0 @@
-import rainbow as rb
-def read_chromatogram(chromatogram_rawdata, chromatogram_csv="./"):
-    datadir = rb.read(chromatogram_rawdata)
-    for f in datadir.datafiles:
-        if f.detector == "UV":
-            f.export_csv(f"{chromatogram_csv}/uv_data.csv")
-            break
